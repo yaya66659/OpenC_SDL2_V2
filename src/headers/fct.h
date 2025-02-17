98f66d1e0287ae308f1135ef7d6f2d1bebe52ff7
Yann <yann55446@gmail.com>
2025-02-17 19:13:08 +0100

    Update fct.h

--- src/headers/fct.h
+++ src/headers/fct.h
@@ -1,3 +1,20 @@
+/*  
+ * Ce fichier fait partie de OpenC_SDL2_V2.  
+ *  
+ * OpenC_SDL2_V2 est un logiciel libre : vous pouvez le redistribuer et/ou le modifier  
+ * selon les termes de la licence GNU General Public License publiée par  
+ * la Free Software Foundation, soit la version 3 de la licence,  
+ * soit (à votre choix) toute version ultérieure.  
+ *  
+ * OpenC_SDL2_V2 est distribué dans l’espoir qu’il sera utile,  
+ * mais SANS AUCUNE GARANTIE ; sans même la garantie implicite  
+ * de COMMERCIALISATION ou D’ADAPTATION À UN OBJET PARTICULIER.  
+ * Voir la Licence Publique Générale GNU pour plus de détails.  
+ *  
+ * Vous devriez avoir reçu une copie de la Licence Publique Générale GNU  
+ * avec OpenC_SDL2_V2. Si ce n’est pas le cas, voir <https://www.gnu.org/licenses/>.  
+ */
+
 #ifndef _FCT_H_
 
 #define _FCT_H_
